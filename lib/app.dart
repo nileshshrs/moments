e8652c38d53bc61fb5657373ce94c6522acef92f
nileshshrs <nileshshr54@gmail.com>
2024-12-12 22:41:39 +0545

    added a way to navigate between reigstration and login screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moments/features/login/views/login_screen.dart';
 import 'package:moments/features/registration/view/registration_screen.dart';
-import 'package:moments/features/splashscreen/splash_screen.dart';
+// import 'package:moments/features/splashscreen/splash_screen.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -14,7 +14,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: RegistrationScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
