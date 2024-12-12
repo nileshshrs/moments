b8be20e82312d5a0b90cd2b875d2fb64b6c42523
nileshshrs <nileshshr54@gmail.com>
2024-12-12 22:25:09 +0545

    added a registration screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moments/features/login/views/login_screen.dart';
-// import 'package:moments/features/registration/view/registration_screen.dart';
+import 'package:moments/features/registration/view/registration_screen.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -13,7 +13,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen(),
+      home: RegistrationScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ lib/features/registration/view/registration_screen.dart
@@ -0,0 +1,208 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final Color primaryColor = Color(0xFF63C57A);
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void register(username, email, password) {
+    print('username: $username, email: $email, password: $password');
+    if (username.isNotEmpty && email.isNotEmpty && password.isNotEmpty) {
+      // Navigator.pushReplacement(
+      //   context,
+      //   MaterialPageRoute(builder: (context) =>),
+      // );
+    }
+    _usernameController.clear();
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 90,
+              ),
+              SizedBox(
+                height: 140,
+                width: double.infinity,
+                child: Image.asset(
+                  "assets/images/logo-light.png",
+                  fit: BoxFit.fill,
+                ),
+              ),
+              SizedBox(
+                child: Text(
+                  "Connect with the world around you.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily:
+                        'Cedarville', // Use the font family defined in pubspec.yaml
+                    color: Colors.black54,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              SizedBox(
+                height: 45.0,
+                child: TextFormField(
+                  controller: _emailController,
+                  cursorColor: primaryColor,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 12.0),
+                    hintStyle: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w300),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              SizedBox(
+                height: 45.0,
+                child: TextFormField(
+                  controller: _usernameController,
+                  cursorColor: primaryColor,
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 12.0),
+                    hintStyle: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w300),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              SizedBox(
+                height: 45.0,
+                child: TextFormField(
+                  controller: _passwordController,
+                  cursorColor: primaryColor,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.w300),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 12.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    register(_usernameController.text, _emailController.text,
+                        _passwordController.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  child: Text(
+                    "Register",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Row(
+                children: const [
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      "OR",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Expanded(child: Divider()),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      overlayColor: WidgetStateProperty.all<Color>(
+                          Colors.transparent), // Disable highlight color
+                    ),
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                          color: primaryColor,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
